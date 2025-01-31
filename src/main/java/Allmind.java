37c47a6edb19f5efd3ec02b49702a1bdeac16f27
DESKTOP-OVSDVEK\Wong Yi Hao <wong2e139@gmail.com>
2025-01-31 09:55:25 +0800

    added echo

--- src/main/java/Allmind.java
+++ src/main/java/Allmind.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class Allmind {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+       /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
         System.out.println("Hello! I'm AllMind, the support system for all mercenaries");
 
         System.out.println("What task would you like me to help you with today?");
 
         System.out.println(" ");
+
+        //echo part
+        String message;
+        Scanner in = new Scanner(System.in);
+        do {
+            message = in.nextLine();
+            System.out.println(message);
+            System.out.println(" ");
+        } while(!message.equals("bye"));
 
         System.out.println("That concludes the briefing,good luck.");
     }
