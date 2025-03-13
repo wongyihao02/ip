04f61f8c4271d335987caad0fe0665a587482ecb
DESKTOP-OVSDVEK\Wong Yi Hao <wong2e139@gmail.com>
2025-03-13 22:15:58 +0800

    final turn

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Storage {
-    private final String filePath;
     private final File file;
+    private final Ui ui;
 
     public Storage(String filePath) {
 
-        this.filePath = filePath;
-//        this.file = new File(filePath);
+
         if (!new File(filePath).exists()) {
             File dir = new File("data");
             dir.mkdir();
@@ -24,10 +25,57 @@ public class Storage {
         } else {
             this.file = new File(filePath);
         }
+        ui = new Ui();
     }
 
-    public String[] readFile() throws FileNotFoundException {
-        Scanner look = new Scanner(file);
-        return look.nextLine().split(" ");
+    /**
+     * reads the data stored in the text file to get the tasks saved from the previous session.
+     * @return arraylist where each element is the call to create a Task in Tasklist
+     */
+    public ArrayList<String> readFile() {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            Scanner look = new Scanner(file);
+            look.useDelimiter(System.lineSeparator());
+
+            while (look.hasNext()) {
+                list.add(look.next());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            ui.printMessage("File not found.");
+        }
+        return list;
+    }
+
+    /**
+     * replaces the content of the text file with the given string.A line seperator is added at the end as well.
+     * @param content The String to ovveride the textfile with.
+     */
+    public void writeReplaceFile(String content) {
+
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(content + System.lineSeparator());
+            writer.close();
+        } catch (IOException e) {
+            ui.printMessage(e.getMessage());
+        }
+
+    }
+
+    /**
+     * adds the given string at the end of the text file.A line seperator is added at the end as well.
+     * @param content The String to be added at the end.
+     */
+    public void writeAppendFile(String content) {
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(content + System.lineSeparator());
+            writer.close();
+        } catch (IOException e) {
+            ui.printMessage(e.getMessage());
+        }
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -3,25 +3,46 @@ import java.util.Scanner;
 public class Ui {
     public Ui() {}
 
+    /**
+     * prints the standard message on start
+     */
     public void printWelcomeMessage (){
         System.out.println("Hello! I'm AllMind, the support system for all mercenaries");
         System.out.println("What task would you like me to help you with today?");
         System.out.println(" ");
     }
 
+    /**
+     * gets the users input as a String
+     * @return the users input
+     */
     public String getUserInput() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         return input;
     }
+
+    /**
+     * gets the users input,prints a message for the user to see first
+     * @param prompt message for user to see before giving input
+     * @return the users input
+     */
     public String getUserInput(String prompt) {
         System.out.print(prompt);
         return getUserInput();
     }
 
+    /**
+     * prints the given string
+     * @param message the string to be printed
+     */
     public void printMessage(String message) {
         System.out.println(message);
     }
+
+    /**
+     * prints the standard end message
+     */
     public void printExitMessage() {
         System.out.println("Briefing over.");
     }
